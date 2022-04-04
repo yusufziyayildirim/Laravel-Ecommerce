3ace783ab89f00fe77680caefa83f6ed31e246e3
yusufziyayildirim <yusuffziyayildirim@gmail.com>
2022-04-04 15:42:01 +0300

    integrating blade template

---
+++ resources/views/backend/layouts/_leftnav.blade.php
@@ -0,0 +1,14 @@
+<ul class="nav flex-column">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">
+      <span data-feather="home"></span>
+      Yönetim Paneli
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{route('users.index')}}">
+      <span data-feather="file"></span>
+      Kullanıcılar
+    </a>
+  </li>
+</ul>
---
+++ resources/views/backend/layouts/backend_theme.blade.php
@@ -0,0 +1,79 @@
+
+<!doctype html>
+<html lang="tr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Admin Panel | @yield('title')</title>
+
+    {{-- <link rel="stylesheet" href="{{asset('css/app.css')}}"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="dashboard.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Laravel E-Commerce</a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+  <div class="navbar-nav">
+    <div class="nav-item text-nowrap">
+      <a class="nav-link px-3" href="#">Çıkış</a>
+    </div>
+  </div>
+</header>
+
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        @include('backend.layouts._leftnav')
+      </div>
+    </nav>
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">@yield('title')</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group me-2">
+                <a href="@yield('btn_url')" class="btn btn-sm btn-outline-@yield('btn_css')">@yield('btn_label')</a>
+            </div>
+        </div>
+
+      </div>
+
+      <h2>@yield('subtitle')</h2>
+
+      @yield('content')
+         
+    </main>
+  </div>
+</div>
+
+  <script src="{{asset('js/app.js')}}"></script>
+ 
+  </body>
+</html>
